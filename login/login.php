2dec0f90fab385e5c80779fb9ec70e7da21b0cf8
zyangee <zyangee2@gmail.com>
2024-11-30 23:24:36 +0900

    Update 로그인 성공 메시지 출력

--- login/login.php
+++ login/login.php
@@ -59,7 +59,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             //로그인 성공 로그
             error_log("로그인 성공 - User: {$userid}, IP: {$_SERVER['REMOTE_ADDR']}");
-            echo '<script>alert("로그인 성공");</script>';
+            $_SESSION['login_message'] = '로그인 성공';
 
             header("Location: ../index.php");
             exit();
